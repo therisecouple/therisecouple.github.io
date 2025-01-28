db6b8cfc3730ea923d3050d7ca692542382e7833
sana venkat <svenkateswararao555@gmail.com>
2025-01-28 18:58:41 +0530

    meta tag changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2756984595"
+    serviceWorkerVersion: "2201986327"
   }
 });
