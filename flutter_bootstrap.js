e4f5ed5ae97b5820be8a9d1ef686f7a5ea6e9248
sana venkat <svenkateswararao555@gmail.com>
2025-06-27 21:58:06 +0530

    color changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3561412969"
+    serviceWorkerVersion: "247586206"
   }
 });
